ed3a2b44b81ebae5b3196b31afd1a23bc1b331e4
Adriano Oliveira <drprado2@gmail.com>
2020-08-17 23:38:55 -0300

    Implementando features do desafio 1.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,43 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repository = {title, url, techs, id: uuid(), likes: 0};
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(x => x.id === id);
+  if(repositoryIndex < 0)
+    return response.status(400).send();
+  const repository = { ...repositories[repositoryIndex], title, url, techs};
+  repositories[repositoryIndex] = repository;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(x => x.id === id);
+  if(repositoryIndex < 0)
+    return response.status(400).send();
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(x => x.id === id);
+  if(repositoryIndex < 0)
+    return response.status(400).send();
+  repositories[repositoryIndex].likes++;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
